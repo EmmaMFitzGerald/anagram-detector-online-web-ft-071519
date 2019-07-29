f4a0ca2311147b22dad7ee437db26108503cc7ac
Emma FitzGerald <emmamariafitzgerald@gmail.com>
2019-07-29 17:10:31 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -7,17 +7,7 @@ class Anagram
   end
   
   def match(anagram_array)
-    comparison_array = []
-    correct_anagram_array = []
-    anagram_array.each do |anagram|
-      new_anagram = anagram.split("")
-      comparison_array << new_anagram
-        if comparison_array.sort == @word.split("").sort 
-          correct_anagram_array << comparison_array.join
-        else 
-          comparison_array.clear
-        end
-    end
+    
   end 
   
 end
